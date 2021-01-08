20ea84174227d66262e17b9e42b7f5583c8dda78
LuckyLucas <lucpic123@gmail.com>
2021-01-08 00:39:25 -0500

    * Fixed stabilized kuiperium block.
    * Added right click functionality to the kuiperium pick

--- src/main/java/com/luckylucas/learningfabric/registry/KuiperiumPickaxe.java
+++ src/main/java/com/luckylucas/learningfabric/registry/KuiperiumPickaxe.java
@@ -1,11 +1,23 @@
 package com.luckylucas.learningfabric.registry;
 
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 
 public class KuiperiumPickaxe extends PickaxeItem {
 
     public KuiperiumPickaxe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        playerEntity.playSound(SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, 0.5F, 2.0F);
+        return TypedActionResult.success(playerEntity.getStackInHand(hand));
     }
 }
